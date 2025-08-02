1e1e7b98f1c50681463306b1e57a86cb5b96c86c
edwardalx <eobkwaku@gmail.com>
2025-08-02 22:42:30 +0100

    Advanced Ex2

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const stored = localStorage.getItem('item');
+    const getListFromLocalStg = stored? JSON.parse(stored):[];
+
+
 const addButton = document.getElementById('add-task-btn');
 const taskInput = document.getElementById('task-input');
 const taskList = document.getElementById('task-list');
@@ -16,16 +20,21 @@ addTask = () =>{
         makeRmBtn.classList.add="remove-btn" ;
         makeRmBtn.textContent = "Remove";
         makeRmBtn.onclick = ()=>{ taskList.removeChild(makeListObj)};
+        makeRmBtn.onclick = ()=>{localStorage.removeItem(makeListObj)}
         makeListObj.appendChild(makeRmBtn);
         taskList.appendChild(makeListObj);
+        localStorage.setItem("items",JSON.stringify(makeListObj));
         taskInput.value = ""
 
     }
 }
+
+
+
 addButton.addEventListener("click",addTask);
 
-taskInput.addEventListener("keypress",(e)=>{
-    if(e.key ==="Enter"){
+taskInput.addEventListener("keypress",(event)=>{
+    if(event.key ==="Enter"){
         return addTask()
     }
 })
